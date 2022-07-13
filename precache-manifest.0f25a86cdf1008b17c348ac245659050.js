1f7bcb18b039f93e15c6b2a82909bd4b82e91e65
Aysel Mammadova <ayselmemmedova9317@gmail.com>
2022-07-14 03:39:04 +0400

    Updates

---
+++ precache-manifest.0f25a86cdf1008b17c348ac245659050.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "81dba7d534e10e60d3e4",
+    "url": "/React-Weather-App/static/css/main.98ee82ae.chunk.css"
+  },
+  {
+    "revision": "81dba7d534e10e60d3e4",
+    "url": "/React-Weather-App/static/js/main.81dba7d5.chunk.js"
+  },
+  {
+    "revision": "1327459d39c5648014a5",
+    "url": "/React-Weather-App/static/js/1.1327459d.chunk.js"
+  },
+  {
+    "revision": "90affcdec194e114bfca",
+    "url": "/React-Weather-App/static/js/runtime~main.90affcde.js"
+  },
+  {
+    "revision": "9efba943b3d80491459e487596f29328",
+    "url": "/React-Weather-App/index.html"
+  }
+];
